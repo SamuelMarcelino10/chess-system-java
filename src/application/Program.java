6c1eb2ddd1ea598225ba34f8599ce6e63aa043e0
SamuelMarcelino10 <samuelmarcelino5678@gmail.com>
2026-02-26 22:01:43 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -45,4 +45,4 @@ public class Program {
     }
 }
 
-//prox aula 120
+//prox aula 121
